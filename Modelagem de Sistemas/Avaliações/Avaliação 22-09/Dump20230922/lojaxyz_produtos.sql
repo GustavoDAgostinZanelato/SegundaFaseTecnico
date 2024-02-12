0315d15bf9270b90b70778724ed5878d9e6bc9c2
Gustavo D'Agostin <133470944+GustavoDAgostinZanelato@users.noreply.github.com>
2024-02-12 15:50:34 -0300

    Add files via upload

---
+++ Modelagem de Sistemas/Avaliações/Avaliação 22-09/Dump20230922/lojaxyz_produtos.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `lojaxyz` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `lojaxyz`;
+-- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: lojaxyz
+-- ------------------------------------------------------
+-- Server version	5.5.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produtos` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `tamanho` int(2) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `quantidade` int(5) NOT NULL,
+  `preco` float NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (4,'Tenis',40,'Azul','Adidas',10,275),(5,'Tenis',36,'Vermelho','Nike',10,285),(111,'Sandália',40,'Preto','Adidas',3,100),(222,'Crocs',33,'Azul','Nike',2,75),(333,'Meia',35,'Branco','Puma',1,12);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-22 17:29:29
